4323a01100f3b56cd514d6b9435bdbe36fa4c6a2
itsrosita <rositaal@marshall.usc.edu>
2022-05-09 19:59:27 +0100

    form component update

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,17 +2,30 @@ import React from 'react';
 
 const Form = () => {
   return (
-    <div>
+    <>
       <form>
         <div className="form-group">
-          <input type="firstname" className="form-control" id="exampleInputFirstName" aria-describedby="emailHelp" placeholder="First name" />
+          <input type="FirstName" className="form-control" id="exampleInputFirstName" placeholder="First name" />
         </div>
         <div className="form-group">
-          <input type="lastname" className="form-control" id="exampleInputLastName" placeholder="Last name" />
+          <input type="LastName" className="form-control" id="exampleInputLastName" placeholder="Last name" />
         </div>
-        <button type="submit" className="btn btn-primary">Register</button>
+        <div className="form-group">
+          <input type="Birthday" className="form-control" id="exampleInputBirthday" placeholder="Birthday DD/MM/YY" />
+        </div>
+        <div className="form-group">
+          <input type="Email" className="form-control" id="exampleInputEmail" placeholder="Email" />
+        </div>
+        <div className="form-group">
+          <input type="Password" className="form-control" id="exampleInputPassword" aria-describedby="passwordHelp" placeholder="Password" />
+          <small id="passwordHelp" class="form-text text-muted">6-character minimum; case sensitive.</small>
+        </div>
+        <div className="form-group">
+          <input type="CarMakeModel" className="form-control" id="exampleInputCarMakeModel" placeholder="Car make and model" />
+        </div>
+        <button type="submit" className="btn btn-dark">Register</button>
       </form>
-    </div>
+    </>
   )
 }
 
