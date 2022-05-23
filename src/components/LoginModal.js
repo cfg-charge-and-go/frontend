a50b94e7bcc9ccab8cdcfb7ffee5ed72706db7df
ayana <ayan.a.k@outlook.com>
2022-05-23 23:24:49 +0100

    updated login modal

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,11 +1,7 @@
 import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-<<<<<<< Updated upstream
-import Button from '../components/buttons/Button';
-=======
 import LoginUserForm from './LoginUserForm';
 
->>>>>>> Stashed changes
 
 const LoginModal = () => {
   const navigate = useNavigate()
@@ -24,21 +20,9 @@ const LoginModal = () => {
           <div className="modal-body">
             <button type="button" className="btn-close d-block ms-auto" data-bs-dismiss="modal" aria-label="Close" ref={dismissButton}></button>
             <h5 className="modal-title mb-3" id="loginModalLabel">Welcome to Charge<span className="text-muted">&amp;</span>Go!</h5>
-<<<<<<< Updated upstream
-            <form onSubmit={handleSubmit}>
-              <div className="form-group mb-3">
-                <input type="email" name="Email" className="form-control rounded-pill" id="Email" placeholder="Email" />
-              </div>
-              <div className="form-group mb-3">
-                <input type="password" name="Password" className="form-control rounded-pill" id="Password" aria-describedby="passwordHelp" placeholder="Password" />
-              </div>
-              <Button title="Login" />
-            </form>
-=======
             <div onSubmit={handleSubmit}>
                 <LoginUserForm />
             </div>
->>>>>>> Stashed changes
           </div>
         </div>
       </div>
