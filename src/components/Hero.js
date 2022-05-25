7c847454158ea53abf443195e05ad8b4e078b7e1
Zhaonan Zhang <nanzhaozhang@gmail.com>
2022-05-26 00:30:42 +0100

    hero nav to welcome page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,14 @@
 import Menu from "./Menu";
 import logo from "../assets/logo.png";
 import "./Hero.css";
+import {NavLink} from 'react-router-dom'
 
 export default function Hero({loggedIn}) {
   return (
     <div className="pt-5 pb-4">
       {loggedIn && <Menu />}
       <h1 className="logo">
-        <img src={logo} alt="Charge and Go logo" className="logo" />
+        <NavLink to="/" className={'home-button'} ><img src={logo} alt="Charge and Go logo" className="logo" /></NavLink>
       </h1>
     </div>
   );
--- src/pages/RouteSuggestion.js
+++ src/pages/RouteSuggestion.js
@@ -1,15 +1,15 @@
-import MapContainer from "../api/MapContainer";
-import Hero from "../components/Hero";
-import Footer from "../components/Footer";
+// import MapContainer from "../api/MapContainer";
+// import Hero from "../components/Hero";
+// import Footer from "../components/Footer";
 
-function RouteSuggestion() {
-  return (
-    <div className="text-center py-5">
-      <Hero loggedIn={true} />
-      <MapContainer searchBarPosition={"bottom"}/>
-      <Footer loggedIn={true} />
-    </div>
-  );
-}
+// function RouteSuggestion() {
+//   return (
+//     <div className="text-center py-5">
+//       <Hero loggedIn={true} />
+//       <MapContainer searchBarPosition={"bottom"}/>
+//       <Footer loggedIn={true} />
+//     </div>
+//   );
+// }
 
-export default RouteSuggestion;
+// export default RouteSuggestion;
